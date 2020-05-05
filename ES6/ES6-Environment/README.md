71d2b15ce6d2d61553e703f340691c6ddfe7ca37
1812869775@qq.com <1812869775@qq.com>
2020-05-05 17:17:01 +0800

    update

--- ES6/ES6-Environment/README.md
+++ ES6/ES6-Environment/README.md
@@ -46,6 +46,20 @@ Webpack参考：前端-构建工具部分，这里讲解Babel的转换
 以后，我们执行如下命令，即可将src/index.js这个 ES6 文件转化为 dist/index.js这个 ES5 文件：
 `npm run build`
 
+7. 实现自动编译
+本地安装 nodemon 模块，开发模式
+```bash
+npm install nodemon --save -D
+```
+简化
+```json
+"scripts": {
+   "build": "babel src/index.js -o dist/index.js",
+   "start": "nodemon -w src --exec npm run build"
+}
+```
+以后使用`npm run start`自动编译
+
 ### 新的声明方式
 ES6对声明的进行了扩展，现在可以有三种声明方式了
 1. var：它是variable的简写，可以理解成变量的意思。
@@ -54,17 +68,88 @@ ES6对声明的进行了扩展，现在可以有三种声明方式了
 
 ### 变量的解构赋值
 
-常见方法：
-```js
-const givenFields = Object.getOwnPropertyNames(req.body);
-if (!requireFields.every(filed => givenFields.includes(filed))) {
-   // 使用 APIError 接口 返回错误信息
-   return next(new APIError("Data missing", "not all required", 400));
-}
-```
 
+### 
+
+整理常见方法(ES6 => ES5)
 ```js
-DataStore.posts.push(newPost);
-const postIndex = DataStore.posts.findIndex((item: any) => item.id == req.params.id);
-DataStore.posts.splice(postIndex, 1);n
+import express from 'express';
+const app = express();
+// express 实例方法
+app.use();
+app.get();
+app.post();
+app.delete();
+app.param();
+app.listen();
+
+req.app.get('env');
+res.send("数据添加成功");
+res.json(newPost); // newPost - 对象
+res.status(err.status).json(err);
+res.json(new PublicInfo("post deleted", 200));
+res.json(new PublicInfo("post added", 200, { post: newPost }));
+res.json(new APIError("Validation Error", "Post not found", 404));
+res.json(new PostDetail(selectPost, selectedTodos, imgURLs));
+res.status(404).json({ status: "failed", message: "post not found" });
+
+
+https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
+// 数组属性
+Array.length
+//用于确定传递的值是否是一个 Array。
+Array.isArray(obj)
+// concat() 方法用于合并两个或多个数组。此方法不会更改现有数组，而是返回一个新数组
+var new_array = old_array.concat(value1[, value2[, ...[, valueN]]])
+
+// indexOf()方法返回在数组中可以找到一个给定元素的第一个索引，如果不存在，则返回-1
+arr.indexOf(searchElement[, fromIndex])
+// lastIndexOf() 方法返回指定元素（也即有效的 JavaScript 值或变量）在数组中的最后一个的索引，如果不存在则返回 -1。从数组的后面向前查找，从 fromIndex 处开始。
+arr.lastIndexOf(searchElement[, fromIndex])
+// join() 方法将一个数组（或一个类数组对象）的所有元素连接成一个字符串并返回这个字符串。如果数组只有一个项目，那么将返回该项目而不使用分隔符
+arr.join([separator])
+// includes() 方法用来判断一个数组是否包含一个指定的值，如果是返回 true，否则false
+array.includes();
+// find() 方法返回数组中满足提供的测试函数的第一个元素的值。否则返回 undefined
+array.find(function(currentValue, index, arr),thisValue)
+// findIndex()方法返回数组中满足提供的测试函数的第一个元素的索引。否则返回-1。
+array.findIndex(function(currentValue, index, arr), thisValue)
+// filter() 方法创建一个新数组, 其包含通过所提供函数实现的测试的所有元素
+array.filter(function(currentValue,index,arr), thisValue)
+// every() 方法测试一个数组内的所有元素是否都能通过某个指定函数的测试。它返回一个布尔值。
+array.every(function(currentValue,index,arr), thisValue)
+// map() 方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值
+array.map(function(currentValue,index,arr), thisValue)
+// reduce() 方法对数组中的每个元素执行一个由您提供的reducer函数(升序执行)，将其结果汇总为单个返回值
+arr.reduce(callback(accumulator, currentValue[, index[, array]])[, initialValue])
+// forEach() 方法对数组的每个元素执行一次给定的函数
+arr.forEach(callback(currentValue,index,array),thisArg])
+
+
+// push() 方法可向数组的末尾添加一个或多个元素，并返回新的长度。
+array.push(item1, item2, ..., itemX)
+// howmany:删除的数量；itemX:要添加到数组的新元素,返回删除的数组
+array.splice(index,howmany,item1,.....,itemX)
+// reverse() 方法将数组中元素的位置颠倒，并返回该数组。数组的第一个元素会变成最后一个，数组的最后一个元素变成第一个。该方法会改变原数组
+arr.reverse()
+// shift() 方法从数组中删除第一个元素，并返回该元素的值。此方法更改数组的长度
+arr.shift()
+// pop()方法从数组中删除最后一个元素，并返回该元素的值。此方法更改数组的长度
+arr.pop()
+// unshift() 方法将一个或多个元素添加到数组的开头，并返回该数组的新长度(该方法修改原有数组)
+unshfit()
+// slice() 方法返回一个新的数组对象，这一对象是一个由 begin 和 end 决定的原数组的浅拷贝（包括 begin，不包括end）。原始数组不会被改变
+slice()
+
+
+
+sort()
+splice()
+toString()
+toLocaleString()
+
+
+
 ```
--- ES6/ES6-Environment/src/index.js
+++ ES6/ES6-Environment/src/index.js
@@ -4,3 +4,197 @@ console.log(jieStr);
 
 var a = "ES6";
 console.log(a);
+
+// 数组Array
+// length属性
+const clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+console.log(clothing);
+
+// includes()
+let site = ['runoob', 'google', 'taobao'];
+console.log(site.includes('runoob')); // true
+console.log(site.includes('baidu'));  // false
+console.log([1, 2, 4].includes(1)); // trtue
+console.log([1, 2, 4].includes(2, 4)); // false
+
+
+// push()
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.push("Kiwi")); // 5
+console.log(fruits);              //  [ "Banana", "Orange", "Apple", "Mango", "Kiwi" ]
+console.log(fruits.push("Hello", "world")); // 7
+console.log(fruits); // Array(7) [ "Banana", "Orange", "Apple", "Mango", "Kiwi", "Hello", "world" ]
+
+
+// splice()
+let spliceArray = ['runoob', 'google', 'taobao'];
+console.log(spliceArray.splice(2, 1));
+console.log(spliceArray.splice(2, 0));
+
+// 移除数组的第三个元素，并添加新元素
+let spliceArray1 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(spliceArray1.splice(2, 1, "Lemon", "Kiwi"));
+console.log(spliceArray1);
+
+// 从第三个位置开始删除数组后的两个元素
+let spliceArray2 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(spliceArray2.splice(2, 2));
+console.log(spliceArray2);
+
+//---------------------------------------------
+// 返回ages数组中年龄大于等于20第一个元素的值
+let ageArray = [3, 10, 20, 30];
+
+function checkAge(age) {
+  return age >= 20;
+}
+
+// 使用find方法
+ageArray.find(checkAge);
+ageArray.find(function checkAge(age) {
+  return age >= 20;
+});
+ageArray.find(age => {
+  return age >= 20;
+});
+// ES6
+// 错误
+ageArray.find(age => { age >= 20 });
+ageArray.find(age => age >= 20);
+//测试
+console.log(ageArray.find(age => age >= 20));
+//-----------------------------------------------
+// find()
+const array1 = [5, 12, 8, 130, 44];
+const found = array1.find(element => element > 12);
+console.log(found); // 130
+// findIndex()
+const array2 = [5, 12, 8, 130, 44];
+const isLargeNUmber = array2.findIndex(element => element > 12);
+console.log(isLargeNUmber); // 3
+// filter()
+const array3 = [5, 12, 8, 130, 44];
+const founds = array1.filter(element => element > 12);
+console.log(founds);  // Array [ 130, 44 ]
+// map()
+const array4 = [1, 4, 8, 9, 19];
+const map1 = array4.map(element => element * 2);
+console.log(map1); // Array(5)[2, 8, 16, 18, 38]
+// every()
+const isBelowThreshold = (currentValue) => currentValue < 40;
+const array5 = [1, 30, 39, 29, 10, 13];
+const array6 = [1, 30, 39, 29, 10, 13, 45];
+const every1 = array5.every(isBelowThreshold);
+const every2 = array6.every(isBelowThreshold);
+console.log(every1); // true
+console.log(every2); // false
+
+// concat()
+const arr1 = ['a', 'b', 'c'];
+const arr2 = ['d', 'e', 'f'];
+const arr3 = arr1.concat(arr2);
+console.log(arr3); // Array(6) [ "a", "b", "c", "d", "e", "f" ]
+
+// forEach()
+const EachArr = [1, 2, 3];
+EachArr.forEach(element => {
+  console.log(element + 1);
+  // 2
+  // 3
+  // 4
+})
+
+// indexOf()
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+console.log(beasts.indexOf('duck'));    // 3
+console.log(beasts.indexOf('duck'), 1); // 2
+console.log(beasts.indexOf('ducksss')); // -1
+
+const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+console.log(animals.indexOf('Dodo'));     // 0
+console.log(animals.lastIndexOf('Dodo')); // 3
+console.log(animals.lastIndexOf('Tiger'));// 1
+
+console.log('---------------------');
+
+
+// lastIndexOf()
+// 使用lastIndexOf注意
+var arry = [2, 5, 9, 2];
+// 从此位置开始逆向查找
+console.log(arry.lastIndexOf(2));   // 3
+// 默认为数组的长度减 1(arr.length - 1)，即整个数组都被查找
+console.log(arry.lastIndexOf(2, 3)); // 3 从位置3逆向查找实际位置
+console.log(arry.lastIndexOf(2, 4)); // 3 从位置3逆向查找实际位置
+console.log(arry.lastIndexOf(2, 2)); // 0 从位置2逆向查找实际位置
+
+console.log(arry.lastIndexOf(7)); // -1
+
+// join()
+// 使用四种不同的分隔符连接数组元素
+var a = ['Wind', 'Rain', 'Fire'];
+console.log(a.join());    // Wind,Rain,Fire
+console.log(a.join(',')); // Wind,Rain,Fire
+console.log(a.join(', ')); // Wind, Rain, Fire
+console.log(a.join('+')); // Wind+Rain+Fire
+console.log(a.join(''));  // WindRainFire
+
+// reduce()
+const reduceArr = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+console.log(reduceArr.reduce(reducer));     // 10
+console.log(reduceArr.reduce(reducer, 5));  // 15
+
+// reduce()应用场景
+// 1.计算数组所有值的和
+var sum = [2, 3, 4].reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0)
+console.log(sum); // 9
+// 2.箭头函数形式
+var total = [2, 3, 4].reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(total); // 9
+// 3.累加对象数组里的值
+var initialValue = 0;
+var sumObject = [{ x: 1, y: "he" }, { x: 2, y: 'll' }, { x: 3, y: "o" }].reduce(
+  (accumulator, currentValue) => accumulator + currentValue.x,
+  initialValue
+);
+
+console.log(sumObject); // 6
+
+// 4.将二维数组转化为一维
+// 5.计算数组中每个元素出现的次数
+// 6.数组去重
+let arrs = [2, 3, 4, 4, 5, 6, 3, 1, 5, 6, 7, 8, 4];
+let result = arrs.sort().reduce((init, current) => {
+  if (init.length === 0 || init[init.length - 1] !== current) {
+    init.push(current);
+  }
+  return init;
+}, []);
+console.log(result);
+
+// 7.功能型函数管道(https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)
+
+// 8.使用 reduce实现map
+console.log([2, 3, 1, 4].reverse()); //  [ 4, 1, 3, 2 ]
+
+const shiftArr = [5, 3, 4];
+const firstElement = shiftArr.shift();
+console.log(firstElement); // 5
+console.log(typeof firstElement); // number
+console.log(shiftArr);    // [3,4]
+
+
+// pop()
+let plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+let popped = plants.pop();
+console.log(popped);        // tomato
+console.log(plants);        // Array(4) [ "broccoli", "cauliflower", "cabbage", "kale" ]
+
+
+const shiftArr1 = [1, 2, 3];
+
+console.log(shiftArr1.unshift(4, 5)); // 5
+console.log(shiftArr1); // [ 4, 5, 1, 2, 3 ]
